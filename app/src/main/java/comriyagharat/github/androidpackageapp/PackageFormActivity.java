d043b59d29cb2df2a591e7f91c23eea8a73503a6
riyagharat <riyagharat@yahoo.com>
2017-06-13 09:47:24 -0400

    Initial Commit

---
+++ app/src/main/java/comriyagharat/github/androidpackageapp/PackageFormActivity.java
@@ -0,0 +1,18 @@
+package comriyagharat.github.androidpackageapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Riya on 6/12/2017.
+ * UNF, N00901846
+ */
+
+public class PackageFormActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_package_form);
+    }
+}
