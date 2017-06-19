c9319744b9201fd74c5e16017d533c5429c30061
riyagharat <riyagharat@yahoo.com>
2017-06-19 16:01:31 -0400

    Working on Package object.

---
+++ app/src/main/java/comriyagharat/github/androidpackageapp/Package.java
@@ -0,0 +1,21 @@
+package comriyagharat.github.androidpackageapp;
+
+/**
+ * Created by Riya on 6/19/2017.
+ * UNF, N00901846
+ */
+
+public static class Package {
+
+    public String trackingNumber;
+    public String checkDelivered;
+    public String dateDelivered;
+    public String dateArrived;
+    public String fromPhone;
+    public String fromCity;
+    public String fromState;
+    public String fromLocation;
+    public String fromRoad;
+    public String fromZip;
+    public String
+}
